ca759a0c485665f3cde8935a9c765b55203d1738
Alex Hirsch <w4rh4wk@bluephoenix.at>
2020-04-24 14:10:58 +0200

    Add missile break off behaviour

--- Assets/Weapons/Missile.cs
+++ Assets/Weapons/Missile.cs
@@ -10,6 +10,8 @@ public class Missile : MonoBehaviour
 
     public float Mobility = 28.0f;
 
+    public float BreakOffAngle = 60.0f;
+
     public Target Target;
 
     public Action OnHit;
@@ -34,13 +36,21 @@ public class Missile : MonoBehaviour
         if (Target)
         {
             var toTarget = Target.transform.position - transform.position;
+
+            // handle break off
+            if (Vector3.Angle(Rigidbody.velocity, toTarget) > BreakOffAngle)
+            {
+                Target = null;
+                OnMiss();
+                return;
+            }
+
             var newRotation = Quaternion.LookRotation(toTarget, transform.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, Mobility * Time.deltaTime);
 
             Rigidbody.velocity = transform.rotation * new Vector3(0.0f, 0.0f, Speed);
         }
     }
-
 
     void OnCollisionEnter(Collision collision)
     {
