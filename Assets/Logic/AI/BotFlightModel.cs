3231bfaffd855942b2c4ca85ad025dd2261d9e7d
Alex Hirsch <w4rh4wk@bluephoenix.at>
2020-05-06 23:23:37 +0200

    Improve bot target tracking

--- Assets/Logic/AI/BotFlightModel.cs
+++ Assets/Logic/AI/BotFlightModel.cs
@@ -13,43 +13,57 @@ public class BotFlightModel : MonoBehaviour
         if (Vector3.Distance(transform.position, Waypoint.transform.position) <= Waypoint.ReachedDistance && Waypoint.Next)
             Waypoint = Waypoint.Next;
 
-        Destination = Waypoint.transform.position;
     }
 
     //////////////////////////////////////////////////////////////////////////
 
     Vector3 Destination;
 
+    Vector3 DestinationPreviousPosition;
+
+    float DestinationSpeed;
+
+    void UpdateDestination()
+    {
+        if (Waypoint)
+            Destination = Waypoint.transform.position;
+
+        DestinationSpeed = Vector3.Distance(Destination, DestinationPreviousPosition) / Time.deltaTime;
+        DestinationPreviousPosition = Destination;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+
     void UpdateRotation()
     {
+        var pitchYawRollRate = FlightModelParams.PitchYawRollRate;
+
+        // Even bots can do highG turns!
+        if (Throttle < -0.5f)
+            pitchYawRollRate *= FlightModelParams.HighGTurnPitchRateModifier;
+
         var toDestination = Destination - transform.position;
         Debug.DrawLine(transform.position, transform.position + toDestination, Color.blue);
 
         if (Vector3.Angle(transform.forward, toDestination) < 20.0f)
         {
             // direct rotation
-
-            Throttle = 0.5f;
-
             var targetRotation = Quaternion.LookRotation(toDestination, transform.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, FlightModelParams.PitchYawRollRate.x * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, pitchYawRollRate.x * Time.deltaTime);
         }
         else
         {
             // roll and pitch
-
-            Throttle = -0.5f;
-
             var toDestinationUp = Vector3.ProjectOnPlane(toDestination, transform.forward).normalized;
 
             Debug.DrawLine(transform.position, transform.position + 5.0f * toDestinationUp, Color.red);
             Debug.DrawLine(transform.position, transform.position + 5.0f * transform.forward, Color.green);
             Debug.DrawLine(transform.position, transform.position + 5.0f * transform.up, Color.green);
 
-            var roll = Limit(Vector3.SignedAngle(transform.up, toDestinationUp, transform.forward), FlightModelParams.PitchYawRollRate.z * Time.deltaTime);
+            var roll = Limit(Vector3.SignedAngle(transform.up, toDestinationUp, transform.forward), pitchYawRollRate.z * Time.deltaTime);
             transform.Rotate(Vector3.forward, roll, Space.Self);
 
-            var pitch = Limit(Vector3.SignedAngle(transform.forward, toDestination, transform.right), FlightModelParams.PitchYawRollRate.x * Time.deltaTime);
+            var pitch = Limit(Vector3.SignedAngle(transform.forward, toDestination, transform.right), pitchYawRollRate.x * Time.deltaTime);
             transform.Rotate(Vector3.right, pitch, Space.Self);
         }
     }
@@ -59,9 +73,36 @@ public class BotFlightModel : MonoBehaviour
     //////////////////////////////////////////////////////////////////////////
 
     float Throttle;
-    float Thrust;
+
+    void UpdateThrottle()
+    {
+        Throttle = 0.0f;
+
+        var toDestination = Destination - transform.position;
+        var angleToDestination = Vector3.Angle(transform.forward, toDestination);
+
+        var distanceToDestination = Vector3.Distance(transform.position, Destination);
+        if (distanceToDestination < 100.0f)
+        {
+            if (Speed < DestinationSpeed)
+                Throttle = 0.3f;
+            else
+                Throttle = -0.3f;
+        }
+        else
+        {
+            if (angleToDestination < 30.0f)
+                Throttle = 0.5f;
+            else if (angleToDestination > 80.0f)
+                Throttle = -0.7f;
+        }
+    }
 
     Vector3 Velocity;
+
+    float Speed => Velocity.magnitude;
+
+    float Thrust;
 
     void UpdateVelocity()
     {
@@ -96,6 +137,10 @@ public class BotFlightModel : MonoBehaviour
     void Update()
     {
         UpdateWaypoint();
+
+        UpdateDestination();
+
+        UpdateThrottle();
 
         UpdateRotation();
 
--- Assets/Logic/AI/Waypoint.cs
+++ Assets/Logic/AI/Waypoint.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    public float ReachedDistance = 50.0f;
-
-    public float StayDistance = 800.0f;
-
     public Waypoint Next;
+
+    public float ReachedDistance = 50.0f;
 }
